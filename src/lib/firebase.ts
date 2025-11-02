04bdf490d133706d762c077f71bf5db3c3bf2346
Gianpaulo Coletti <info@remotebusinesspartner.com.au>
2025-11-02 07:30:29 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "studio-8897114742-ff06b",
+  "appId": "1:1056433687745:web:d66780c0394fe87424d840",
+  "apiKey": "AIzaSyC6iIy9ZrEBTfpf6jDDPzKOe64YBQG6qYk",
+  "authDomain": "studio-8897114742-ff06b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1056433687745"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
