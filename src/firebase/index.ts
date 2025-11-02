7a7cadec095bb9d86f43c79f056927a5e5826073
info-rbp <info@remotebusinesspartner.com.au>
2025-11-02 08:08:44 -0800

    Stop connecting to Firebase emulators in development

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -24,8 +24,8 @@ export function getSdks(firebaseApp: FirebaseApp) {
         // IMPORTANT: Do not move these connect calls outside of the DEV block.
         // They should only run in development.
         console.log("Connecting to local Firebase emulators.");
-        connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
-        connectFirestoreEmulator(firestore, 'localhost', 9098);
+        // connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
+        // connectFirestoreEmulator(firestore, 'localhost', 9098);
     } catch (e: any) {
         // The "already-initialized" errors are expected if hot-reloading.
         if (e.code !== 'auth/emulator-config-failed' && e.code !== 'firestore/emulator-config-failed') {
